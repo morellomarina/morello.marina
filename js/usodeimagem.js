9070c227499e08c9c797c834fb61051b50f5ebfb
morellomarina <43457451+morellomarina@users.noreply.github.com>
2018-11-08 21:30:20 -0200

    Update usodeimagem.js

--- js/usodeimagem.js
+++ js/usodeimagem.js
@@ -58,7 +58,7 @@ const contratoUsoDeImagemABI = [
 	}
 ];
 
-var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0xcb5eb5a839b07d97faf2103c133e5f078123d5e7");
+var contratoUsoDeImagem = web3.eth.contract(contratoUsoDeImagemABI).at("0xe0c40a9a754a5fde99524a61a51cae9f133e0510");
 
 function obtemNomeEmpresa() {
     contratoUsoDeImagem.nomeEmpresa({from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
